987470c2b3d3d314321e4206ddd48b1311b5984b
Ahiati Emmanuel <ahiatiemmanuel77@gmail.com>
2025-03-02 20:54:35 +0000

    Conditional Statements

---
+++ 2.10 Conditionals/Conditionals.js
@@ -0,0 +1,8 @@
+
+let answer = prompt("What is the official name of Javascript");
+ (answer == "ECMAScript") ? alert("Right!") : alert("You don’t know? ECMAScript!");
+ 
+let numberEntered = prompt('Enter a value');
+(numberEntered > 0) ? alert("The number you entered is greater than zero") :
+ (numberEntered < 0) ? alert("The number is less than zero") :
+alert("The number equals zero");
