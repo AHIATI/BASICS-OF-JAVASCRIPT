1ebe58b8b48718a2823e2a31ff15c6e19fb65ad1
Ahiati Emmanuel <ahiatiemmanuel77@gmail.com>
2025-03-02 20:54:57 +0000

    Data types

---
+++ 2.5 Data types/dataTypes.js
@@ -0,0 +1,19 @@
+/* there are eight data types in js
+number for any type of number
+bigInt for large numbers
+string for strings or single characters
+boolean, null for null variables and undefined and symbol the undefined is when a variable is declared but not assigned
+
+  With string we can three types of quotes that is the single quotes, double quotes and back tick single quotes
+ Backticks are “extended functionality” quotes. They allow us to embed variables and expressions into a string by wrapping them in ${…}, for example: */
+  
+
+ let name = "Ahiati";
+ let x = 5;
+ alert(x++);
+ alert(x);
+
+ alert(`Hello ${name}`);
+ alert( `hello ${"name"}`);
+ alert (`Hello ${1}`);
+ 
